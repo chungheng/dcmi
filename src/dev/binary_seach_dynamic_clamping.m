fa2692e4e05612f0417380f14c9de1a151246d36
Chung-Heng Yeh <chungheng.yeh@gmail.com>
2013-05-20 16:45:10 -0400

    Succesful experiments on counterbalancing leaky and potassium channel

--- src/dev/binary_seach_dynamic_clamping.m
+++ src/dev/binary_seach_dynamic_clamping.m
@@ -5,17 +5,40 @@ addpath('../hhn/');
 dt = 1e-5;
 t  = -0.1:dt:2;
 I  = 0*ones(size(t));
-
+gi = -1;
+g  = -0.3;
 [V M S] = hodgkin_huxley_BSDC(t,I,'Verbose',true,...
-          'memcon', @mem_con,'MemInitState',[0 0 0],'MaxCon',[0 0 -1.0],'MinCon',[0 0 0]);
+          'memcon', @mem_con,'MemInitState',[0 0 0],'MaxCon',[0 0 gi],'MinCon',[0 0 0]);
 %%
 figure();
     subplot(3,1,1);
         plot(t*1e3,V(:,1));xlim(1e3*[t(1) t(end)]);ylabel('Membrane voltage, [mV]');
         ylim([-100 100]);
     subplot(3,1,2);
-        plot(t*1e3,M(:,3));xlim(1e3*[t(1) t(end)]);ylim([-1.1 0.1]);
-        hold on;plot( 1e3*[t(1) t(end)], [-0.3 -0.3],'--r');ylabel('Injected Conducatance');
+        plot(t*1e3,M(:,3));xlim(1e3*[t(1) t(end)]);ylim([gi-0.1 0.1]);
+        hold on;plot( 1e3*[t(1) t(end)], [g g],'--r');ylabel('Injected Conducatance');
+        legend('Injected','Original','Location','SouthEast');
+    subplot(3,1,3);
+        plot(t*1e3,S(:,1),'g');xlim(1e3*[t(1) t(end)]);
+        hold on;plot(t*1e3,S(:,2)-1.5,'k');xlim(1e3*[t(1) t(end)]);
+        ylim([-2.1 1.6]);ylabel('Contral Signal');set(gca,'YTick',[]);xlabel('time, [ms]');
+        legend('Reset Signal','Breakdown Indicator','Location','SouthEast');
+        
+%% Identification of leaky channel using binary search dynamic clamping
+dt = 1e-5;
+t  = -0.1:dt:2;
+I  = 0*ones(size(t));
+gi = -60;
+g  = -36;
+[V M S] = hodgkin_huxley_BSDC(t,I,'Verbose',true,...
+          'memcon', @mem_con,'MemInitState',[0 0 0],'MaxCon',[0 gi 0],'MinCon',[0 0 0]);
+figure();
+    subplot(3,1,1);
+        plot(t*1e3,V(:,1));xlim(1e3*[t(1) t(end)]);ylabel('Membrane voltage, [mV]');
+        ylim([-100 100]);
+    subplot(3,1,2);
+        plot(t*1e3,M(:,2));xlim(1e3*[t(1) t(end)]);ylim([gi-0.1 0.1]);
+        hold on;plot( 1e3*[t(1) t(end)], [g g],'--r');ylabel('Injected Conducatance');
         legend('Injected','Original','Location','SouthEast');
     subplot(3,1,3);
         plot(t*1e3,S(:,1),'g');xlim(1e3*[t(1) t(end)]);
