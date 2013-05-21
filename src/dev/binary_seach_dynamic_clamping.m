3a62e533fb9bd5d339747008f5c662eeef7dcfc1
Chung-Heng Yeh <chungheng.yeh@gmail.com>
2013-05-20 21:06:07 -0400

    First experiment of sodium channel identification
    - problem with initial value of maximum conductance value

--- src/dev/binary_seach_dynamic_clamping.m
+++ src/dev/binary_seach_dynamic_clamping.m
@@ -24,7 +24,7 @@ figure();
         ylim([-2.1 1.6]);ylabel('Contral Signal');set(gca,'YTick',[]);xlabel('time, [ms]');
         legend('Reset Signal','Breakdown Indicator','Location','SouthEast');
         
-%% Identification of leaky channel using binary search dynamic clamping
+%% Identification of potassium channel using binary search dynamic clamping
 dt = 1e-5;
 t  = -0.1:dt:2;
 I  = 0*ones(size(t));
@@ -45,3 +45,25 @@ figure();
         hold on;plot(t*1e3,S(:,2)-1.5,'k');xlim(1e3*[t(1) t(end)]);
         ylim([-2.1 1.6]);ylabel('Contral Signal');set(gca,'YTick',[]);xlabel('time, [ms]');
         legend('Reset Signal','Breakdown Indicator','Location','SouthEast');
+        
+%% Identification of sodium channel using binary search dynamic clamping
+dt = 1e-5;
+t  = -0.1:dt:2;
+I  = 0*ones(size(t));
+gi = -140;
+g  = -120;
+[V M S] = hodgkin_huxley_BSDC_sodium(t,I,'Verbose',true,...
+          'memcon', @mem_con,'MemInitState',[0 0 0],'MaxCon',[gi -18 0],'MinCon',[0 -36 0]);
+figure();
+    subplot(3,1,1);
+        plot(t*1e3,V(:,1));xlim(1e3*[t(1) t(end)]);ylabel('Membrane voltage, [mV]');
+        ylim([-100 100]);
+    subplot(3,1,2);
+        plot(t*1e3,M(:,1));xlim(1e3*[t(1) t(end)]);ylim([gi-0.1 0.1]);
+        hold on;plot( 1e3*[t(1) t(end)], [g g],'--r');ylabel('Injected Conducatance');
+        legend('Injected','Original','Location','SouthEast');
+    subplot(3,1,3);
+        plot(t*1e3,S(:,1),'g');xlim(1e3*[t(1) t(end)]);
+        hold on;plot(t*1e3,S(:,2)-1.5,'k');xlim(1e3*[t(1) t(end)]);
+        ylim([-2.1 1.6]);ylabel('Contral Signal');set(gca,'YTick',[]);xlabel('time, [ms]');
+        legend('Reset Signal','Decreasing Indicator','Location','SouthEast');
