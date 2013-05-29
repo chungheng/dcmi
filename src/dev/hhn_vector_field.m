a747eef9e2bf5798470a8f28869a65acc157c0bf
Chung-Heng Yeh <chungheng.yeh@gmail.com>
2013-05-29 17:18:24 -0400

    Add scaling of 'n' in vector field calculation to achieve better rendering

--- src/dev/hhn_vector_field.m
+++ src/dev/hhn_vector_field.m
@@ -1,12 +1,14 @@
 function [DV DN] = hhn_vector_field(V,N,g)
+    s = (max(max(V)) - min(min(V))) / (max(max(N)) - min(min(N)));
     H = 1-N;
     M =  0.1*(V+40) ./ (1-exp(-(V+40)/10)) / 4 ./ exp(-(V+65)/18);
     DN = zeros(size(V));
     a  = exp(-(V+55)/10)-1;
     DN( a==0 ) = ( 1-N( a==0 ) ) * 0.1 - N( a==0 ) .* (0.125.*exp(-(V( a==0 ) +65)/80));
     DN( a~=0 ) = (1-N( a~=0 )) .* (-0.01*(V(a~=0)+55)./a(a~=0)) - N( a~=0 ) .* (0.125*exp(-(V(a~=0)+65)/80));
-    DV = - g(1)*M.^3.*H.*(V-50) - g(2)*N.^4.*(V+77) - g(3)*(V+54.387); 
+    DN = DN*s;
+    DV = (- g(1)*M.^3.*H.*(V-50) - g(2)*N.^4.*(V+77) - g(3)*(V+54.387)); 
     b = sqrt(DN.^2 + DV.^2);
-    DN = DN./b/10;
-    DV = DV./b/10;
+    DN = DN./b/s;
+    DV = DV./b;
 end
