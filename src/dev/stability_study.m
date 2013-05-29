c187f7906a4407ce1aaffaa0ecbc4c56d574f2be
Chung-Heng Yeh <chungheng.yeh@gmail.com>
2013-05-29 17:20:01 -0400

    Finished the stability study of the leaky channel
    - compute steady state for gL > 0
    - plot vector field for gl = -0.3, 0, 0.3, 0.6
    - generate a short clip of vector field for different value of gL

--- src/dev/stability_study.m
+++ src/dev/stability_study.m
@@ -1,41 +1,105 @@
-%% 
-ss =  [-64.9964    0.3177    0.0530    0.5960];
-g  =  [120 36 0.3];
-[x fval flag] = fsolve( @(x) hhn(x,g), ss );
-
-%% 
-% Use Rinzel's model to reduce the model to 2 variable V and n, and plot the 
-% corresponding vector field
-g  =  [120 36 0.3];
-v = -70:-60;
-n = 0.25:.01:0.35;
-[V, N] = meshgrid(v,n);
-
-[DV DN] = hhn_vector_field(V, N, g);
-figure();quiver(V,N,DV,DN,0.1);
+%% Stability study of pseudo leaky channel injection
 %%
+% Plot the null-solution for gL of different value. There is no solution 
+% when gL is negative
+ss =  [-64.9964    0.3177    0.0530    0.5960];
 gL = -1:.05:1;
 X  = zeros(numel(gL), 4);
+f  = zeros(1,numel(gL));
 for i = 1:numel(gL)
     [x fval flag] = fsolve( @(x) hhn(x,[120 36 gL(i)]), ss );
-    if flag == 1
-        X(i,:)  = x;
-    end
+    X(i,:)  = x;
+    f(i) = flag;
 end
+idx = find(f == 1);
 
-figure();plot3(X(:,1),X(:,2),gL);grid on;
+le = cell(1,numel(idx)-1);
+figure();cs = colormap( hsv( numel(idx)+1 ) );
+
+for i = 1:numel(idx)-1
+    hold on;plot(X(idx(i:i+1),1),X(idx(i:i+1),2),'color',cs(i,:));
+    le{i} = num2str(gL(idx(i)));
+end
+grid on;axis([-100 -55 0 0.5]);legend(le,'Location','NorthWest');
+xlabel('Voltage, [mV]');ylabel('N');
 %%
-ss =  [-64.9964    0.3177    0.0530    0.5960];
-g  =  [120 36 0.3];
+% Use Rinzel's model to reduce the model to 2 variable V and n, and plot 
+% the corresponding vector field. The vector field for different value of
+% gL is cascaded into a gif movie.
+v = -100:2:-60;
+n = 0:0.02:0.5;
+[V, N] = meshgrid(v,n);
 
-g_hat = -1:0.1:1;
-E = zeros(numel(g_hat),4);
 
-for i = 1:numel(g_hat)
-    E(i,:) = HH_Jacobian(ss,[120 36 g_hat(i)]);
+gL = -1:.05:1;
+
+figure();
+filename = 'gl_vf.gif';
+for i = 1:numel(gL)
+    set(gcf,'Color','W');
+    [DV DN] = hhn_vector_field(V, N, [120 36 gL(i)]);
+    plot(X(idx,1),X(idx,2),'k');
+    hold on;plot( ss(1), ss(2), 'go','Markerfacecolor','g');
+    if any(idx == i)
+        hold on;plot( X(i,1), X(i,2), 'ro','Markerfacecolor','r');
+        le = {'Trace of Steady State',...
+              'Steady state of the oringinal system',...
+              'Steady state of the new system' };
+    else
+        le = {'Trace of Steady State','Steady state of the oringinal system'};
+    end
+    
+    quiver(V,N,DV,DN,0.5,'Marker','o','ShowArrowHead','Off');
+    xlim([-105 -55]);ylim([0 0.5]);title(['g*_L=',num2str(gL(i),'%3.2f')]);
+    legend(le);
+    drawnow
+    frame = getframe(1);
+    im = frame2im(frame);
+    [imind,cm] = rgb2ind(im,256);
+    if i == 1;
+        imwrite(imind,cm,filename,'gif', 'Loopcount',inf,'DelayTime',0.2);
+    else
+        imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime',0.2);
+        clf
+    end    
 end
-figure();cs = colormap(hsv(7));
-for i = 1:4
-    hold on;plot(real(E(:,i)),imag(E(:,i)),'color',cs(i,:));
-end
+
+%% 
+% Highlight the vector field 
+v = -100:2:-60;
+n = 0:0.02:0.5;
+[V, N] = meshgrid(v,n);
+% gL = 0.6
+gl = 0.6;
+[DV DN] = hhn_vector_field(V, N, [120 36 gl]);
+idx = find( abs(gL-gl)<0.01, 1, 'first');
+figure();plot( X(idx,1) , X(idx,2) ,'ro',ss(1),ss(2),'go');
+         hold on;quiver(V,N,DV,DN,0.5,'Marker','o','ShowArrowHead','Off');
+         xlim([-105 -55]);ylim([0 0.5]);title(['g*_L=',num2str(gl,'%3.2f')]);
+         legend('Steady state of the new system','Steady state of the oringinal system');
+% gL = 0.3
+gl = 0.3;
+[DV DN] = hhn_vector_field(V, N, [120 36 gl]);
+idx = find( abs(gL-gl)<0.01, 1, 'first');
+figure();plot( X(idx,1) , X(idx,2) ,'ro',ss(1),ss(2),'go');
+         hold on;quiver(V,N,DV,DN,0.5,'Marker','o','ShowArrowHead','Off');
+         xlim([-105 -55]);ylim([0 0.5]);title(['g*_L=',num2str(gl,'%3.2f')]);
+         legend('Steady state of the new system','Steady state of the oringinal system');
+% gL = 0
+gl = 0.0;
+[DV DN] = hhn_vector_field(V, N, [120 36 gl]);
+idx = find( abs(gL-gl)<0.01, 1, 'first');
+figure();plot( X(idx,1) , X(idx,2) ,'ro',ss(1),ss(2),'go');
+         hold on;quiver(V,N,DV,DN,0.5,'Marker','o','ShowArrowHead','Off');
+         xlim([-105 -55]);ylim([0 0.5]);title(['g*_L=',num2str(gl,'%3.2f')]);
+         legend('Steady state of the new system','Steady state of the oringinal system');
+% gL = 0.3
+gl = -0.3;
+[DV DN] = hhn_vector_field(V, N, [120 36 gl]);
+idx = find( abs(gL-gl)<0.01, 1, 'first');
+figure();plot(ss(1),ss(2),'go');hold on;
+         quiver(V,N,DV,DN,0.5,'Marker','o','ShowArrowHead','Off');
+         %hold on;plot( X(idx,1) , X(idx,2) , 'ro');
+         xlim([-105 -55]);ylim([0 0.5]);title(['g*_L=',num2str(gl,'%3.2f')]);
+         legend('Steady state of the oringinal system');
 
